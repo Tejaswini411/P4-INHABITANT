bc823584c98796824f085e34bd4bb13af80a8319
[kumartejaswini4@gmail.com] <tejaswinikumar@Tejaswinis-MacBook-Pro.local>
2020-01-14 20:34:17 -0800

    about page updated

--- themes/inhabitant/header.php
+++ themes/inhabitant/header.php
@@ -5,7 +5,9 @@
  * @package RED_Starter_Theme
  */
 
-?><!DOCTYPE html>
+?>
+
+<!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
@@ -16,42 +18,47 @@
 	<?php wp_head(); ?>
 	</head>
 
-	<body class="home page-template-default page page-id-4">
-		<div id="page" class="hfeed site">
+	<body class="home-page">
+		<div class="hfeed site">
 			<a class="skip-link screen-reader-text" href="#content">Skip to content</a>
 
-						<header id="masthead" class="site-header reverse-header" role="banner">
-				<div class="container">
-					<div class="site-branding">
-						<div class="logo">
-							<a href="" rel="home">
-								<h1 class="site-title">Inhabitent</h1>
-							</a>
-						</div>
-					</div><!-- .site-branding -->
-
-					<nav id="site-navigation" class="main-navigation clearfix" role="navigation">
-						<div class="menu-main-navigation-container"><ul id="primary-menu" class="menu"><li id="menu-item-143" class="menu-item menu-item-type-post_type_archive menu-item-object-product menu-item-143"><a href=" ">Shop</a></li>
-<li id="menu-item-18" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-18"><a href="">Journal</a></li>
-<li id="menu-item-21" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-21"><a href="<?php echo esc_url(home_url('/')); ?>about">About</a></li>
-<li id="menu-item-17" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-17"><a href="">Find Us</a></li>
-</ul></div>						<div class="header-search">
-							<form role="search" method="get" class="search-form" action="">
-	<fieldset>
-		<a href="#" class="search-toggle" aria-hidden="true">
-			<i class="fa fa-search"></i>
-		</a>
-		<label>
-			<input type="search" class="search-field" placeholder="Type and hit enter..." value="" name="s" title="Search for:" />
-		</label>
-		<input type="submit" id="search-submit" class="screen-reader-text" value="Search" />
-	</fieldset>
-</form>
-						</div>
-					</nav><!-- #site-navigation -->
+	<header id="masthead" class="site-header" role="banner">
+			<div class="container">
+					<a class="header-logo" href="<?= get_site_url(); ?>">
+						<img class="header-logo-img" src="<?= get_template_directory_uri() ?>/images/logos/inhabitent-logo-tent-white.svg" style="display: <?php echo $displaywhite ?>;">
+					   <!--  <img class="header-logo-img" src="<?= get_template_directory_uri() ?>/images/logos/inhabitent-logo-tent.svg" style="display: <?php echo $displayblue ?>;"> -->
+					</a>
 				</div>
-			</header>
-	 
+		
+
+<nav class="site-navigation" role="navigation">
+<div class="main-navigation-container">
+	<ul class="menu">
+			<li class="list-item "><a href="<?php echo esc_url(home_url('/')); ?>product_type">Shop</a></li>
+			<li class="list-item"><a href="<?php echo esc_url(home_url('/')); ?>journal">Journal</a></li>
+			<li class="list-item "><a href="<?php echo esc_url(home_url('/')); ?>about">About</a></li>
+			<li class="list-item "><a href="">Find Us</a></li>
+	</ul>
+</div>						
+
+
+<div class="header-search">
+<form role="search" method="get" class="search-form" action="">
+		<fieldset>
+			<a href="#" class="search-toggle" aria-hidden="true">
+				<i class="fa fa-search"></i>
+			</a>
+			<label>
+				<input type="search" class="search-field" placeholder="Type and hit enter..." value="" name="s" title="Search for:" />
+			</label>
+			<input type="submit" id="search-submit" class="screen-reader-text" value="Search" />
+		</fieldset>
+</form>
+</div>
+
+</nav><!-- #site-navigation -->
+</div>
+</header>
 	
 
 
--- themes/inhabitant/page-about.php
+++ themes/inhabitant/page-about.php
@@ -7,12 +7,11 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="content-area about">
 		<main id="main" class="site-main" role="main">
-
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+				<?php get_template_part( 'template-parts/content', 'about' ); ?>
 
 			<?php endwhile; // End of the loop. ?>
 
